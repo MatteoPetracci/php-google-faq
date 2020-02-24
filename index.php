077b000af78385d4c015ed33084769af74bdea06
Matteo <matteop18@gmail.com>
2020-02-24 14:18:44 +0100

    header senza sass

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    <div class="box">
+      <img src="img/googlelogo.png" alt="">
+      <h2><a href="#">Privacy e termini</a></h2>
+    </div>
+    <div class="box">
+      <nav>
+        <div class="container">
+          <ul>
+            <li>Introduzione</li>
+            <li>Norme sulla privacy</li>
+            <li>Termini di servizi</li>
+            <li>Tecnologie</li>
+            <li class="blue">Domande frequenti</li>
+          </ul>
+        </div>
+        <div class="container">
+          <a href="">Account Google</a>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <main>
+
+  </main>
+</body>
+</html>
