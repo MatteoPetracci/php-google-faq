62fe5244a8d0dc74aa6eaa6df85cc2c7d51ac0b7
Matteo <matteop18@gmail.com>
2020-02-24 15:13:59 +0100

    stampo a schermo le chiavi dell'array db

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $db = [
     'response'=> "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
   ]
 ];
-var_dump($db);
+// var_dump($db);
 ?>
 
 <!DOCTYPE html>
@@ -58,7 +58,11 @@ var_dump($db);
     </div>
   </header>
   <main>
-
+    <section>
+      <?php foreach ($db as $key => $value) { ?>
+        <h4><?php echo $key; ?></h4>
+      <?php } ?>
+    </section>
   </main>
 </body>
 </html>
